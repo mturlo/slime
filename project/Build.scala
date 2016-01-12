6e22c21029af87f21ec9e345809d8d2ad3fea27e
mwielocha <mikolaj.wielocha@yougov.com>
2016-01-12 15:44:07 +0100

    add image_url to attachement

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object SlimeBuild extends Build {
   }
 
   val projectName         = "slime"
-  val projectVersion      = "0.1.1-SNAPSHOT"
+  val projectVersion      = "0.1.2-SNAPSHOT"
 
   val projectDependencies = Seq(
     "org.java-websocket" % "Java-WebSocket" % V.javaWebsockets withSources(),
